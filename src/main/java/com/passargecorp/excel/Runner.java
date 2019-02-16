c7c2ef79335b502dba06e6f5c6c6246c1a268d32
Michael Passarge <mpassarge@comcast.net>
2019-02-16 18:24:36 -0500

    Added logback logging

--- src/main/java/com/passargecorp/excel/Runner.java
+++ src/main/java/com/passargecorp/excel/Runner.java
@@ -3,13 +3,23 @@ package com.passargecorp.excel;
 import com.passargecorp.excel.workbook.Work;
 import com.passargecorp.excel.workbook.output.OutputInterface;
 import com.passargecorp.excel.workbook.output.UniqueOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
  */
 public class Runner {
+
+    public static final Logger LOG = LoggerFactory.getLogger(Runner.class);
+
     public static void main(String[] args) {
+
+        LOG.info("Starting with {} arguments", args);
+        // TODO: 2/16/19 add Input from command line apache commons cli
         OutputInterface output = new UniqueOutput("S2 Lauren");
+
         Work.runner("T", "/home/mpassarge/Downloads/inter-rater-reliability-dev.xlsx", output);
+        LOG.info("Finished.");
     }
 }
--- src/main/java/com/passargecorp/excel/workbook/Work.java
+++ src/main/java/com/passargecorp/excel/workbook/Work.java
@@ -4,6 +4,8 @@ import com.passargecorp.excel.workbook.output.OutputInterface;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -12,6 +14,7 @@ import java.io.IOException;
 public class Work {
 
     private final static int DEFAULT_SHEET = 0;
+    private static Logger LOG = LoggerFactory.getLogger(Work.class);
 
     private static Workbook workbook;
     private static Sheet sheet;
@@ -19,16 +22,17 @@ public class Work {
     private Work() {/**/}
 
     private static boolean columnInSheet(final int colNumber) {
-        return colNumber < sheet.getLastRowNum();
+        return colNumber < sheet.getRow(0).getLastCellNum();
     }
 
     private static boolean loadBook(String filePath) {
+        LOG.info("Loading work book from file \"{}\"", filePath);
         try {
             workbook = WorkbookFactory.create(new File(filePath));
             workbook.setMissingCellPolicy(Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
             return true;
         } catch (IOException e) {
-            // TODO: 2/15/19 Log error message
+            LOG.error("Could not read from file \"{}\"", filePath);
             return false;
         }
     }
@@ -37,30 +41,29 @@ public class Work {
 
         final boolean bookLoaded = loadBook(filePath);
         if (bookLoaded) {
-
+            LOG.info("Workbook loaded...");
             final int colNumber = CellReference.convertColStringToIndex(columnCoordinate);
-
             sheet = workbook.getSheetAt(DEFAULT_SHEET);
             if (columnInSheet(colNumber)) {
+                LOG.info("Editing Column {} with {}", columnCoordinate, output.getValue());
                 for (Row row : sheet) {
                     final Cell cell = row.getCell(colNumber);
                     if (cell.getCellType().equals(CellType.BLANK)) {
                         output.setValue(cell);
                     }
                 }
-            } else {
-                // TODO: 2/15/19 Log error message
             }
+            outputWorkbook(filePath);
         }
-        outputWorkbook(filePath);
     }
 
     private static void outputWorkbook(final String filePath) {
+        LOG.info("Saving workbook");
         try {
             saveOldWorkbook(filePath);
             saveNewWorkbook(filePath);
         } catch (IOException e) {
-            // TODO: 2/16/19 log error message
+            LOG.error("Error saving workbook at \"{}\"", filePath);
         }
     }
 
